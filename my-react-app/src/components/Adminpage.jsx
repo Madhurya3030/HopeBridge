7398ed7603724e920a94043e2eb283bf2c0d3cd0
Madhurya <111988941+Madhurya3030@users.noreply.github.com>
2025-03-23 09:01:50 +0530

    Final Commit

--- my-react-app/src/components/Adminpage.jsx
+++ my-react-app/src/components/Adminpage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./AdminPage.css";
+import "./Adminpage.css";
 
 const AdminPage = () => {
   const [reports, setReports] = useState([]);
